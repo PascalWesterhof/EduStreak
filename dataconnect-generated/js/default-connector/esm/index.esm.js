35aa38a000e23bd9aec54f1ae23c4277925897ad
Julian Woo <julian.woo@student.nhlstenden.com>
2025-05-23 10:38:03 +0200

    firebase setup en begin van cloud messaging

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'edustreak',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'edustreak',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
