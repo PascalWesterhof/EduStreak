5971139475e992e6774e36136a28970cf0146923
David Klein <davidklein2611@gmail.com>
2025-06-13 16:48:38 +0200

    Firebase API keys verplaatst naar .env bestand

--- EduStreak/config/firebase.js
+++ EduStreak/config/firebase.js
@@ -5,15 +5,16 @@ import { browserLocalPersistence, getReactNativePersistence, initializeAuth } fr
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { Platform } from 'react-native';
+import Config from "react-native-config";
 
 // Ensure you have these environment variables set in your EAS build profile (and locally for development if needed)
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDe6kTo9XiAJu6mZ2gmZJiJI4MyuqAnMA',
-  authDomain: 'edustreak-772c2.firebaseapp.com',
-  projectId: 'edustreak-772c2',
-  storageBucket: 'edustreak-772c2.firebasestorage.app',
-  messagingSenderId: '538149596527',
-  appId: '1:538149596527:web:d0de85d9379e2959fc8c9a',
+  apiKey: Config.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: Config.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: Config.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: Config.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
@@ -31,7 +32,7 @@ const storage = getStorage(app);
 
 // Configure Google Sign-In
 GoogleSignin.configure({
-  webClientId: '538149596527-1dgue7rp6omuknv7kannrttjc04u35vg.apps.googleusercontent.com',
+  webClientId: Config.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
 });
 
 export { app, auth, db, storage };
